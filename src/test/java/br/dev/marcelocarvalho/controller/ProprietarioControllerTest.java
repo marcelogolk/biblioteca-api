489d53e46e0d2e3d40a75cacc8b16078e21846ff
marcelogolk <marcelo.g.olk@gmail.com>
2026-09-22 18:14:01 -0300

    versão 0.1 CRUD terminado.

---
+++ src/test/java/br/dev/marcelocarvalho/controller/ProprietarioControllerTest.java
@@ -0,0 +1,38 @@
+package br.dev.marcelocarvalho.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProprietarioControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void listAllProprietarios() {
+    }
+
+    @Test
+    void buscarProprietarioById() {
+    }
+
+    @Test
+    void incluirProprietario() {
+    }
+
+    @Test
+    void atualizarProprietario() {
+    }
+
+    @Test
+    void excluirProprietario() {
+    }
+}
---
+++ src/test/java/br/dev/marcelocarvalho/service/LivroServiceTest.java
@@ -0,0 +1,28 @@
+package br.dev.marcelocarvalho.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LivroServiceTest {
+
+    @Test
+    void listAllLivros() {
+    }
+
+    @Test
+    void buscarLivroById() {
+    }
+
+    @Test
+    void incluirLivro() {
+    }
+
+    @Test
+    void atualizarLivro() {
+    }
+
+    @Test
+    void deletarLivroById() {
+    }
+}
